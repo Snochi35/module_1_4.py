aa50d22b3647a1777ee0c790e02e5bda802d8bef
Snochi35 <levanianoize@gmail.com>
2024-09-11 16:40:00 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+#1
+my_string = (str(input("what i study? ")))
+number_of_characters = (len(my_string))
+print(number_of_characters)
+#2
+print(my_string.upper())
+print(my_string.upper().lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+# =D
